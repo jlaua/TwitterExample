abd44ef956d8498fd5ede3683812dffb5870a856
jlaua <jlaua@hotmail.com>
2015-08-05 20:23:48 -0500

    Estado Inicial V1

--- app/src/main/java/pe/edu/cibertec/twitterexample/MainActivity.java
+++ app/src/main/java/pe/edu/cibertec/twitterexample/MainActivity.java
@@ -1,38 +1,57 @@
 package pe.edu.cibertec.twitterexample;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
-public class MainActivity extends ActionBarActivity {
+import io.fabric.sdk.android.Fabric;
+
+
+public class MainActivity extends AppCompatActivity  {
+
+    public static final String CONSUMER_KEY = "nKpYTK5thtOO5pschZzIWNJSQ";
+    public static final String CONSUMER_SECRET = "C6U5Cwf8bSOBczJApnB0G9Dz0F2uDBpLswe3zAiDSfVgqhDQOL";
+    private static final String TAG = "MainActivity";
+    private TwitterLoginButton btnTwitterLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(CONSUMER_KEY,CONSUMER_SECRET);
+        Fabric.with(this,new TwitterCore(authConfig));
+        TwitterLoginButton btnTwitteLoginButton = (TwitterLoginButton) findViewById(R.id.btnTwitterLogin);
+        btnTwitteLoginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                Log.d(TAG,"Login sucess");
+            }
+
+            @Override
+            public void failure(TwitterException e) {
+                Log.e(TAG,"Login Error ",e );
+
+            }
+        });
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        btnTwitterLogin.onActivityResult(requestCode,resultCode,data);
     }
 }
